65feecf7f8f1ddddf7d6ae7945bf7b6a531eaf77
Shougo13 <Korona4649@gmail.com>
2022-10-11 16:38:12 +0900

    [3の倍数]を学習しました。

---
+++ 02_レシピ/01_3の倍数/main.js
@@ -0,0 +1,9 @@
+const genkiFunction = function (number) {
+  for (let i = 1; i <= number; i++) {
+    if (i % 3 === 0) {
+      console.log(i + "!!!!!!!")
+    } else {
+      console.log(i)
+    }
+  }
+}
